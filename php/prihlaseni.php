d0b65b5d740ed81d0b3b6d8ac9ea5e7384c369cc
Coolheim <kulheimmatej@gmail.com>
2024-05-01 13:54:25 +0200

    aadsfadsf

--- php/prihlaseni.php
+++ php/prihlaseni.php
@@ -1,3 +1,20 @@
+<?php
+    require 'connection.php';
+
+    if(isset($_POST["submit"])){
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $query = "INSERT INTO user_info VALUES('','$username','$email','$password')";
+        mysqli_query($conn,$query);
+
+        echo "<script> alert('Data Inserted Successfully'); </script>";
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,11 +32,12 @@
         <a href="../index.php" class="btn1">Domů</a>
     </div>
     <div class="container">
-        <form>
-            <input type="text" placeholder="Uživatelské jméno"><br>
-            <input type="email" placeholder="E-mail"><br>
-            <input type="password" placeholder="Password"><br>
-            <input type="submit">
+        <form class="" action="" method="post" autocomplete="off">
+            <input type="text" name="username" required value="" placeholder="Uživatelské jméno"><br>
+            <input type="email" name="email" required value="" placeholder="E-mail"><br>
+            <input type="password" name="password" required value="" placeholder="Password"><br>
+            <!-- <button type="submit" name="submit">Submit</button> -->
+            <input type="submit" name="submit">
         </form>
     </div>
     </main>
